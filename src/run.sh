581e4804319c93a1104a3c4bb2cc3c88dd72ae74
Jason Kimko <22348009+jskimko@users.noreply.github.com>
2017-09-08 01:15:29 -0400

    Sort files if necessary.
            - Fix std_names path in config.

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,11 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
-# Read configuration file
+###########################
+#       READ CONFIG       #
+###########################
+
+echo -n "Reading configuration file... "
 data1=`grep data1 $1 | awk -F'"' '{print $2}'`
 data2=`grep data2 $1 | awk -F'"' '{print $2}'`
 names1=`grep names1 $1 | awk -F'"' '{print $2}'`
@@ -20,19 +24,56 @@ min_age2=`grep min_age2 $1 | awk -F'"' '{print $2}'`
 max_age2=`grep max_age2 $1 | awk -F'"' '{print $2}'`
 
 # Check if we have everything
-function check_valid {
+function not_empty {
     if [ -z "$2" ]; then
         echo "run.sh:error: invalid config entry '$1'" >&2
         exit
     fi
 }
-check_valid data1     $data1     ; check_valid data2    $data2;
-check_valid names1    $names1    ; check_valid names2   $names2;
-check_valid std_names $std_names ; check_valid sex      $sex;
-check_valid year1     $year1     ; check_valid year2    $year2;
-check_valid min_age1  $min_age1  ; check_valid max_age1 $max_age1;
-check_valid min_age2  $min_age2  ; check_valid max_age2 $max_age2;
+not_empty data1     "$data1"     ; not_empty data2    "$data2";
+not_empty names1    "$names1"    ; not_empty names2   "$names2";
+not_empty std_names "$std_names" ; not_empty sex      "$sex";
+not_empty year1     "$year1"     ; not_empty year2    "$year2";
+not_empty min_age1  "$min_age1"  ; not_empty max_age1 "$max_age1";
+not_empty min_age2  "$min_age2"  ; not_empty max_age2 "$max_age2";
+echo "Done."
 
 #echo $data1; echo $data2; echo $names1; echo $names2; echo $std_names;
 #echo $year1; echo $year2; echo $sex;
 #echo $min_age1; echo $max_age1; echo $min_age2; echo $max_age2;
+
+
+#####################
+#       SORT        #
+#####################
+
+# Sort data and name files
+echo -n "Sorting files if necessary... "
+for f in "$data1" "$data2" "$names1" "$names2"; do
+    if [ ! -f "$f" ]; then
+        echo "run.sh:error: invalid path '$f'" >&2
+        exit
+    fi
+
+    if [ ! -f "$f.sort" ]; then
+        path=`readlink -f "$f" | sed 's:\(.*\)/.*:\1:'`
+        file=`readlink -f "$f" | sed 's:.*/\(.*\):\1:'`
+        echo "Sorting $file..."
+        time sort -T $HOME -snk2 "$f" > $path/$file.sort
+    fi
+done
+
+# Sort std_names
+if [ ! -f "$std_names" ]; then
+    echo "run.sh:error: invalid path '$f'" >&2
+    exit
+fi
+
+if [ ! -f "$f.sort" ]; then
+    path=`readlink -f "$std_names" | sed 's:\(.*\)/.*:\1:'`
+    file=`readlink -f "$std_names" | sed 's:.*/\(.*\):\1:'`
+    echo "Sorting $file..."
+    time sort -T $HOME -sk1.1,1.1 "$std_names" > $path/$file.sort
+fi
+echo "Done."
+
